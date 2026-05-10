5a2101c2ec1fcb03463b756868d47c3d69f9c51f
rafeu15min <rafaeldepaulo7@hotmail.com>
2026-05-10 01:40:49 -0300

    Teclado (PIC)

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,8 +1,38 @@
-use crate::println;
+use crate::{gdt, println}; // Importe o gdt aqui!
 use lazy_static::lazy_static;
+use pc_keyboard::{DecodedKey, HandleControl, Keyboard, ScancodeSet1, layouts};
+use pic8259::ChainedPics;
+use spin::Mutex;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: Mutex<ChainedPics> =
+    Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+// Enum para mapear os fios físicos da placa-mãe (IRQs)
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+    Keyboard,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+}
+
 lazy_static! {
+    static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                layouts::Us104Key,
+                HandleControl::Ignore,
+            ));
+
     // Assim como o VGA, a IDT precisa ser global e estática
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
@@ -10,6 +40,16 @@ lazy_static! {
         // Cadastramos a nossa função personalizada para lidar com o choque do Breakpoint
         idt.breakpoint.set_handler_fn(breakpoint_handler);
 
+        // Sem essas linhas, a GDT e o handler ficam isolados e geram os avisos.
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt[InterruptIndex::Timer.as_u8()]
+                    .set_handler_fn(timer_interrupt_handler);
+
+        idt[InterruptIndex::Keyboard.as_u8()]
+            .set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -26,4 +66,54 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     // Usamos a nossa macro global recém-criada para exibir o erro
     println!("EXCECAO CAPTURADA: BREAKPOINT");
     println!("{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    crate::print!(fg: crate::vga_buffer::Color::Red, "\n[FALHA CATASTRÓFICA] ");
+    crate::println!(fg: crate::vga_buffer::Color::White, "DOUBLE FAULT");
+    crate::println!("{:#?}", stack_frame);
+
+    loop {}
+}
+
+// --- TRATADOR DO RELÓGIO (Timer) ---
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    // Imprime um ponto a cada tique do relógio
+    crate::print!(".");
+
+    // Regra de Ouro: Avisa o PIC que terminamos de processar
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+// --- TRATADOR DO TECLADO ---
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use x86_64::instructions::port::Port;
+
+    // Lemos a Porta de hardware 0x60 (onde o teclado cospe os elétrons)
+    let mut port = Port::new(0x60);
+    let scancode: u8 = unsafe { port.read() };
+
+    let mut keyboard = KEYBOARD.lock();
+
+    // Passamos o pulso elétrico para a máquina de estado traduzir para ASCII
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => crate::print!("{}", character),
+                DecodedKey::RawKey(key) => crate::print!("{:?}", key),
+            }
+        }
+    }
+
+    // Regra de Ouro: Libera o teclado para a próxima tecla
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,27 +20,24 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    // O texto normal continua saindo em amarelo
     print!("Inicializando o Kernel ");
-
-    // Injetamos a cor apenas na palavra Heimdall!
     print!(fg: Color::LightCyan, "Heimdall");
-
-    // Como a cor é restaurada automaticamente, o resto volta a ser amarelo natural
     println!("...");
 
-    // 1. O Heimdall envia a Tabela de Interrupções para o Hardware
     gdt::init();
     interrupts::init_idt();
-    println!("Tabela de Interrupcoes (IDT) carregada com sucesso.");
 
-    // 2. Disparamos o Breakpoint manualmente direto no silício!
-    // Isso emula o processador gritando por causa de um choque.
-    x86_64::instructions::interrupts::int3();
+    // 1. Inicia o chip controlador PIC
+    unsafe { interrupts::PICS.lock().initialize() };
 
-    // 3. Se a IDT funcionar, o processador vai pausar, exibir a mensagem amarela
-    // do nosso handler, e DEPOIS VOLTAR PARA ESTA LINHA e continuar rodando!
-    println!(fg: Color::LightGreen, "Sobrevivemos ao Breakpoint! O Kernel continua rodando.");
+    // 2. Abre as comportas do processador (Instrução 'sti')
+    x86_64::instructions::interrupts::enable();
 
-    loop {}
+    println!("Hardware ativado. Escutando interrupcoes externas...");
+
+    // O loop infinito final (O Kernel fica ocioso aguardando teclas)
+    loop {
+        // Halt pausa a CPU (economizando energia) até a próxima interrupção chegar!
+        x86_64::instructions::hlt();
+    }
 }
